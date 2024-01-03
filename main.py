90c591bceffb47cea863c5bda11e4ab167111787
YMD0123 <nasebanaru.t23@gmail.com>
2024-01-04 01:11:46 +0900

    testtestst

--- main.py
+++ main.py
@@ -10,18 +10,10 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    str_box = message.content
-    if message.author.bot:
-        return 
-    elif str_box[0:19] == "https://twitter.com":
-        x_url = "https://vxtwitter.com/" + message.content[20:]
-        await message.channel.send(x_url)
-    elif str_box[0:13] == "https://x.com":
-        x_url = "https://vxtwitter.com/" + message.content[14:]
-        await message.channel.send(x_url)
-    
+    emoji ="👍"
+    await message.add_reaction(emoji)
+
 TOKEN = os.getenv("DISCORD_TOKEN")
-#Web サーバの立ち上げ
+# Web サーバの立ち上げ
 keep_alive()
 client.run(TOKEN)
