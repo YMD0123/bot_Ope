1eec03478faa998845021d7b043a6b2c146ad0cc
YMD0123 <nasebanaru.t23@gmail.com>
2024-01-04 23:32:17 +0900

    URL GO git add .

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.bot:
-      return
-    elif str_box[0:19] == "https://twitter.com":
-      x_url = "https://vxtwitter.com/" + message.content[20:]
-      await message.channel.send(x_url)
-    elif str_box[0:13] == "https://x.com":
-      x_url = "https://vxtwitter.com/" + message.content[14:]
-
+  str_box = message.content
+  if message.author.bot:
+    return
+  elif str_box[0:19] == "https://twitter.com":
+    x_url = "https://vxtwitter.com/" + message.content[20:]
+    await message.channel.send(x_url)
+  elif str_box[0:13] == "https://x.com":
+    x_url = "https://vxtwitter.com/" + message.content[14:]
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 # Web サーバの立ち上げ
