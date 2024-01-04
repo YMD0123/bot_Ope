c5df9fcaaa553a5bf7ecdf389697e8747a1f86f1
YMD0123 <nasebanaru.t23@gmail.com>
2024-01-04 23:44:55 +0900

    hi!

--- main.py
+++ main.py
@@ -13,11 +13,8 @@ async def on_message(message):
   str_box = message.content
   if message.author.bot:
     return
-  elif str_box[0:19] == "https://twitter.com":
-    x_url = "https://vxtwitter.com/" + message.content[20:]
-    await message.channel.send(x_url)
-  elif str_box[0:13] == "https://x.com":
-    x_url = "https://vxtwitter.com/" + message.content[14:]
+  elif message.content == "/hi":
+    await message.channel.send("hi")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 # Web サーバの立ち上げ
