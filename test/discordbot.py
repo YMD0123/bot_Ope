4b6b35f4ada1a52522aefb9862b46d1ca97601fb
YMD0123 <nasebanaru.t23@gmail.com>
2024-01-04 16:19:24 +0900

    go

--- test/discordbot.py
+++ test/discordbot.py
@@ -24,6 +24,8 @@ async def on_message(message):
     str_box = message.content
     if message.author.bot:
         return 
+    elif message.content == "/hi":
+        await message.channel.send("hi")
     elif str_box[0:19] == "https://twitter.com":
         x_url = "https://vxtwitter.com/" + message.content[20:]
         await message.channel.send(x_url)
