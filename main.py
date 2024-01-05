a433cf886287e82c819e9d0ed7e704ec8a1f3588
YMD0123 <nasebanaru.t23@gmail.com>
2024-01-05 23:32:49 +0900

    go

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import discord
 import os
 from keep_alive import keep_alive
-from logging import getLogger
+
+class MyClient(discord.Client):
+  #メッセージが書き込まれた時
+  async def on_message(self, message):
+    str_box = message.content
+    #送信者がbot自身の場合はコマンドを無効にする
+    if message.author.bot:
+      return
+    elif "/stop" in message.content:
+      #ログオフ
+      await message.channel.send("Goodbye")
+      await self.close()
+      return 
+    elif message.content == "/hi":
+      await message.channel.send("hi")
+    elif str_box[0:19] == "https://twitter.com":
+      x_url = "https://vxtwitter.com/" + message.content[20:]
+      await message.channel.send(x_url)
+    elif str_box[0:13] == "https://x.com":
+      x_url = "https://vxtwitter.com/" + message.content[14:]
 
 
-client = discord.Client(intents=discord.Intents.default())
 
-@client.event
-async def on_ready():
-  print('ログインしました')
+def main():
+    #環境変数からtokenを取ってくる
+  TOKEN = os.getenv("DISCORD_TOKEN")
+  keep_alive()
+  #すべての機能を使えるようにする
+  intents = discord.Intents.all()
+  #intentsは必須パラメータ
+  client = MyClient(intents=intents)
+  #Discord接続
+  client.run(TOKEN)
 
-@client.event
-async def on_message(message):
-  str_box = message.content
-  if message.author.bot:
-    return
-  if message.content == "/hi":
-    await message.channel.send("hi")
-
-logger = getLogger(__name__)
-logger.info('message')
-TOKEN = os.getenv("DISCORD_TOKEN")
-# Web サーバの立ち上げ
-keep_alive()
-client.run(TOKEN)
+if __name__ == "__main__":
+  
+  # Web サーバの立ち上げ
+  main()
