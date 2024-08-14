d9243fbc4b8edc4575b922af66716160bc5030f8
YMD0123 <nasebanaru.t23@gmail.com>
2024-08-14 17:27:20 +0900

    新式 改

--- main.py
+++ main.py
@@ -1,92 +1,111 @@
 import discord
-import os
-from keep_alive import keep_alive
+import os, datetime, time
 
-client = discord.Client(intents=discord.Intents.default())
-
-@client.event
-async def on_ready():
-    print('ログインしました')
-
-@client.event
-async def on_message(self, message):
-  if message.author.bot:
-    return
-  elif "/stop" in message.content:
+class MyClient(discord.Client):
+  #botクラスの処理内容
+  async def on_message(self, message):
+    if message.author.bot:
+      return
+    elif "/stop" in message.content:
     #ログオフ
-    await message.channel.send("Goodbye")
-    await self.close()
-    return 
-  
-  str_box = message.content
-  channel1 = self.get_channel(1196743997465575424) #logチャンネル
+      await message.channel.send("Goodbye")
+      await self.close()
+      return 
+
+    str_box = message.content
+    channel1 = self.get_channel(1196743997465575424) #logチャンネル
+
+
     #送信者がbot自身の場合はコマンドを無効にする
-  if message.author.bot:
-    return
+    if message.author.bot:
+      return
     
     #/stopコマンド＝bot停止
-  elif "/stop" in message.content:
-    #ログオフ
-    await message.channel.send("Goodbye")
-    await self.close()
-    return 
+    elif "/stop" in message.content:
+      #ログオフ
+      await message.channel.send("Goodbye")
+      await self.close()
+      return 
     
     #/hiコマンド＝存在確認
-  elif message.content == "/hi":
-    await message.channel.send("hi")  
-    if(channel1 != None):
-      await channel1.send("hi")
+    elif message.content == "/hi":
+      await message.channel.send("hi")  
+      if(channel1 != None):
+        await channel1.send("hi")
 
-  #https://twitter.comがある場合
-  elif str_box.find("https://twitter.com") != -1:
-    #https://twitter.comの位置を取得
-    x_point = str_box.find("https://twitter.com")
-    #https://vxtwitter.com/の後ろのURLを取得
-    x_url  = "https://vxtwitter.com/" + message.content[x_point+20:]
-    #URLが3つ以下の場合は無効
-    if(x_url.count('/') <= 3):
-      return
-    #URLを送信
-    await message.channel.send(x_url)
-    #送信者を送信
-    await message.channel.send(message.author)
-    #https://twitter.comが先頭の場合は削除
-    if(x_point == 0):
-      await message.delete()
-    #ログチャンネルがある場合
-    if(channel1 != None):
-      #URLを送信
-      await channel1.send(x_url)
-      #送信者を送信
-      await channel1.send(message.author)
+    #https://twitter.comが
+    elif str_box.find("https://twitter.com") != -1:
+      x_point = str_box.find("https://twitter.com")
+      x_url  = "https://vxtwitter.com/" + message.content[x_point+20:]
+      if(x_url.count('/') <= 3):
+        return
+      await message.channel.send(x_url)
+      await message.channel.send(message.author)
+      if(x_point == 0):
+        await message.delete()
+      if(channel1 != None):
+        await channel1.send(x_url)
+        await channel1.send(message.author)
 
 
 
 
-  elif str_box.find("https://x.com") != -1:
-    #https://x.comの位置を取得
-    x_point = str_box.find("https://x.com")
-    #https://vxtwitter.com/の後ろのURLを取得
-    x_url  = "https://vxtwitter.com/" + message.content[x_point+14:]
-    #URLが3つ以下の場合は無効
-    if(x_url.count('/') <= 3):
-      return
-    #URLを送信
-    await message.channel.send(x_url)
-    #送信者を送信
-    await message.channel.send(message.author)
-    #https://x.comが先頭の場合は削除
-    if(x_point == 0):
-      await message.delete()
-    #ログチャンネルがある場合
-    if(channel1 != None):
-      #URLを送信
-      await channel1.send(x_url)
-      #送信者を送信
-      await channel1.send(message.author)
+    elif str_box.find("https://x.com") != -1:
+      x_point = str_box.find("https://x.com")
+      x_url  = "https://vxtwitter.com/" + message.content[x_point+14:]
+      if(x_url.count('/') <= 3):
+        return
+      await message.channel.send(x_url)
+      await message.channel.send(message.author)
+      if(x_point == 0):
+        await message.delete()
+      if(channel1 != None):
+        await channel1.send(x_url)
+        await channel1.send(message.author)
 
+        
+def main():
+  #タイムゾーン調整
+  tz_jst = datetime.timezone(datetime.timedelta(hours=9))
 
-TOKEN = os.getenv("DISCORD_TOKEN")
-# Web サーバの立ち上げ
-keep_alive()
-client.run(TOKEN)
+  #すべての機能を使えるようにする
+  intents = discord.Intents.all()
+  #クラス生成、intentsは必須パラメータ
+  client = MyClient(intents=intents)
+
+  try:
+    #Discord接続(エラー発生用)
+    client.run("ABC")
+
+    if (client.is_closed()):
+      #discord.errors.HTTPException: 429 Too Many Requests対策
+      SysClose()
+    
+  except discord.DiscordException as e:
+    errorTime = str(datetime.datetime.now(tz_jst))
+    errorText = "エラー発生:" + errorTime + "\n"
+    errorText += str(e) + "\n"
+
+    print("\n" + errorText)
+    #エラーログを保存
+    LogWrite(errorText)
+    #discord.errors.HTTPException: 429 Too Many Requests対策
+    SysClose()
+
+#discord.errors.HTTPException: 429 Too Many Requests対策
+def SysClose():
+  print("osを切ります")
+  os.system("kill 1")
+
+#エラーログを保存する
+def LogWrite(logText):
+  sleepTime = 12
+  textFile = open('log.txt', 'a')
+  textFile.write(logText)
+  textFile.close()
+  print("処理が終わるまで待ちます:" + str(sleepTime) + "秒")
+  #更新が終わるまで待たせる(早いと更新が正常に終わらない)
+  time.sleep(sleepTime)
+
+if __name__ == "__main__":
+  main()
